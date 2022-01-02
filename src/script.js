41aa0075b295c343bfcaf71eae102e18c0dafb4d
Maniteja <78750315+katammaniteja@users.noreply.github.com>
2022-01-02 10:48:59 +0530

    changes added

--- src/script.js
+++ src/script.js
@@ -1,114 +1,138 @@
-let music = document.querySelector("audio");
-let play = document.getElementById("play");
-let img = document.querySelector("img");
-let artist = document.getElementById("artist");
-let title = document.getElementById("title");
-let prev = document.getElementById("prev");
-let next = document.getElementById("next");
-let progress = document.getElementById("progress");
-let current_time = document.getElementById("current_time");
-let duration_time = document.getElementById("duration");
-let progress_div=document.getElementById("progress_div");
+var audio_volume=0.6;
 
-let songList=[
-    {
-        songName:"In My Feelings",
-        songArtist:"Drake",
-    },
-    {
-        songName:"Faded",
-        songArtist:"Alan Walker",
-    },
-    {
-        songName:"Let you love me",
-        songArtist:"Rita Ora",
-    },
-    {
-        songName:"Without Me",
-        songArtist:"Halsey",
-    },
-    {
-        songName:"Be Alright",
-        songArtist:"Dean Lewis",
-    },
-    {
-        songName:"Go Bang",
-        songArtist:"Pnau",
-    },
-]
-
-let isPlaying = false;
-play.addEventListener('click', () => {
-    if (!isPlaying) {
-        music.play();
-        play.classList.replace("fa-play", "fa-pause");
-        img.classList.add("anime");
-        isPlaying = true;
-        play.title = "Pause";
-    } else {
-        music.pause();
-        play.classList.replace("fa-pause", "fa-play");
-        img.classList.remove("anime");
-        isPlaying = false;
-        play.title = "Play";
-    }
-});
-
-let i = 1;
-
-function loadSong(i) {
-    music.src = `src/songs/${i}.mp3`;
-    img.src = `src/covers/${i}.jpg`;
-    title.innerText=songList[i-1].songName;
-    artist.innerText=songList[i-1].songArtist;
-    progress.style.width = `0%`;
-    if(!isPlaying){
-        isPlaying=true;
-        play.classList.replace("fa-play", "fa-pause");
-        img.classList.add("anime");
-    }
-    music.play();
+const animate = (key) => {
+    const currentKey = document.querySelector(`.${key}`);
+    if(!currentKey) return;
+    currentKey.classList.add('pressed');
+    setTimeout(() => {
+        currentKey.classList.remove('pressed');
+    }, 300);
 }
-prev.addEventListener('click', () => {
-    i--;
-    if (i == 0) i = 6;
-    loadSong(i);
-})
-next.addEventListener('click', () => {
-    i++;
-    if (i == 7) i = 1;
-    loadSong(i);
-})
-music.addEventListener('ended', () => {
-    i++;
-    if (i == 7) i = 1;
-    loadSong(i);
+
+const playMusic=(path) =>{
+    const audio=new Audio(path);
+    audio.volume=audio_volume;
+    audio.play();
+}
+
+document.addEventListener("keypress",(event)=>{
+    const triggeredKey=event.key;
+    makeSound(triggeredKey);
+    animate(triggeredKey);
 })
 
-// Progress JS Work
-music.addEventListener('timeupdate',  (event)=> {
-    const {
-        currentTime,
-        duration
-    } = event.srcElement;
 
-    let progress_time = (currentTime / duration) * 100;
-    progress.style.width = `${progress_time}%`;
+//theme 1
+const theme1_background="#091921";
+const theme1_text="#00fff1";
 
-    let duration_min = parseInt(duration / 60);
-    let duration_sec = parseInt(duration % 60);
-    if (duration_sec < 10) duration_sec = '0' + duration_sec;
-    if(duration){
-        duration_time.textContent = `${duration_min}:${duration_sec}`;
+
+//theme2
+const theme2_background="#f7c340";
+const theme2_text="#2d2d2d";
+
+const changeTheme=(theme)=>{
+    let root=document.documentElement;
+    if(theme==="theme1"){
+        root.style.setProperty('--background',theme1_background);
+        root.style.setProperty('--text',theme1_text);
+        
     }
+    else{
+        root.style.setProperty('--background',theme2_background);
+        root.style.setProperty('--text',theme2_text);
+    }
+}
 
-    let currentMin = parseInt(currentTime / 60);
-    let currentSec = parseInt(currentTime % 60);
-    if (currentSec < 10) currentSec = '0' + currentSec;
-    current_time.textContent = `${currentMin}:${currentSec}`;
+var curr_theme="theme1";
+const theme_changer=document.getElementById("util_button-theme");
+theme_changer.addEventListener("click",(e)=>{
+    theme_changer.classList.add('change_theme');
+    setTimeout(() => {
+        theme_changer.classList.remove('change_theme');
+    }, 300);
+
+
+    if(curr_theme=="theme1"){
+        changeTheme("theme2");
+        curr_theme="theme2";
+    }
+    else{
+        changeTheme("theme1");
+        curr_theme="theme1";
+    }
 })
 
-progress_div.addEventListener('click',(event)=>{
-    let new_time=(event.offsetX/event.srcElement.clientWidth)*(music.duration);
-    music.currentTime=new_time;
+
+
+var auto_music_id;
+var auto_music_on=false;
+const startAutoMusic=()=>{
+    const letters=['w','a','s','d','j','k','l'];
+    auto_music_id=setInterval(() => {
+        const selectedKey=letters[Math.floor(Math.random()*letters.length)];
+        makeSound(selectedKey);
+        animate(selectedKey); 
+    },300 );
+};
+
+const auto_music_button=document.getElementById("util_button-auto");
+auto_music_button.addEventListener('click',()=>{
+    if(auto_music_on){
+        clearInterval(auto_music_id);
+        auto_music_on=false;
+        auto_music_button.classList.remove('pressed');
+        auto_music_button.textContent='Start Auto Music';
+    }
+    else{
+        auto_music_on=true;
+        startAutoMusic();
+        auto_music_button.classList.add('pressed');
+        auto_music_button.textContent='Stop Auto Music';
+    }
+})
+
+const slider=document.getElementById("volume_slider");
+slider.oninput=(event)=>{
+    audio_volume= event.target.value/100;
+}
+
+const makeSound = (key) => {
+    switch (key) {
+        case 'w':
+            playMusic("src/sounds/sound-1.mp3");
+            break;
+        case 'a':
+            playMusic("src/sounds/sound-2.mp3");
+            break;
+        case 's':
+            playMusic("src/sounds/sound-3.mp3");
+            break;
+        case 'd':
+            playMusic("src/sounds/sound-4.mp3");
+            break;
+        case 'j':
+            playMusic("src/sounds/sound-5.mp3");
+            break;
+        case 'k':
+            playMusic("src/sounds/sound-6.mp3");
+            break;
+        case 'l':
+            playMusic("src/sounds/sound-7.mp3");
+            break;
+        default:
+            console.log("Hey wrong button");
+    }
+}
+
+const handleDrumClick = (event) => {
+    var innerHTML = event.target.innerHTML;
+    console.log(innerHTML);
+    animate(innerHTML);
+    makeSound(innerHTML);
+}
+
+var drums = document.querySelectorAll('.drum');
+drums.forEach((element) => {
+    element.addEventListener('click', handleDrumClick);
 })
